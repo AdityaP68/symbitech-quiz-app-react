1093b27408f0c4d56adf04b7c78ffb0d00047e21
FlameBuster <53975239+FlameBuster@users.noreply.github.com>
2022-10-09 20:17:01 +0530

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by {" "}
+      <a
+        href="https://gist.github.com/SyedaMahamFahim"
+        style={{ cursor: "pointer" }}
+      >
+        Syeda Maham Fahim
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
