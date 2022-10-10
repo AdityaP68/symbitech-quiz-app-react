89481664e2db5882c11f200a7f565175b030a3be
Aditya Prakash <adityaprakash6688@gmail.com>
2022-10-10 16:34:27 +0530

    questions error handling}

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -48,76 +48,73 @@ const Question = ({
     setScore(0);
   };
   // console.log("ques", questions);
-  console.log(currQues, questions.length)
 
   return (
     <div className="question">
       <h1>Question {currQues + 1} :</h1>
       {error && <ErrorMessage>{error}</ErrorMessage>}
-      {currQues < questions.length-1 && (
-        <div className="singleQuestion">
-          <h2>{questions[currQues].description}</h2>
-          <div className="options">
-            <button
-              className={`singleOption ${selected === 1 && "select"}`}
-              onClick={() => {
-                handleCheck(1);
-              }}
-              disabled={selected}
-            >
-              {questions[currQues].option_a}
-            </button>
-            <button
-              className={`singleOption ${selected === 2 && "select"}`}
-              onClick={() => {
-                handleCheck(2);
-              }}
-              disabled={selected}
-            >
-              {questions[currQues].option_b}
-            </button>
-            <button
-              className={`singleOption ${selected === 3 && "select"}`}
-              onClick={() => {
-                handleCheck(3);
-              }}
-              disabled={selected}
-            >
-              {questions[currQues].option_c}
-            </button>
-            <button
-              className={`singleOption ${selected === 4 && "select"}`}
-              onClick={() => {
-                handleCheck(4);
-              }}
-              disabled={selected}
-            >
-              {questions[currQues].option_d}
-            </button>
-          </div>
-          <div className="controls">
-            <Button
-              variant="contained"
-              color="secondary"
-              size="large"
-              style={{ width: 185 }}
-              href="/"
-              onClick={() => handleQuit()}
-            >
-              Quit
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              size="large"
-              style={{ width: 185 }}
-              onClick={handleNext}
-            >
-              {currQues > questions.length - 2 ? "Submit" : "Next Question"}
-            </Button>
-          </div>
+      {questions && questions[currQues] && <div className="singleQuestion">
+        <h2>{questions[currQues].description}</h2>
+        <div className="options">
+          <button
+            className={`singleOption ${selected === 1 && "select"}`}
+            onClick={() => {
+              handleCheck(1);
+            }}
+            disabled={selected}
+          >
+            {questions[currQues].option_a}
+          </button>
+          <button
+            className={`singleOption ${selected === 2 && "select"}`}
+            onClick={() => {
+              handleCheck(2);
+            }}
+            disabled={selected}
+          >
+            {questions[currQues].option_b}
+          </button>
+          <button
+            className={`singleOption ${selected === 3 && "select"}`}
+            onClick={() => {
+              handleCheck(3);
+            }}
+            disabled={selected}
+          >
+            {questions[currQues].option_c}
+          </button>
+          <button
+            className={`singleOption ${selected === 4 && "select"}`}
+            onClick={() => {
+              handleCheck(4);
+            }}
+            disabled={selected}
+          >
+            {questions[currQues].option_d}
+          </button>
         </div>
-      )}
+        <div className="controls">
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            style={{ width: 185 }}
+            href="/"
+            onClick={() => handleQuit()}
+          >
+            Quit
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            style={{ width: 185 }}
+            onClick={handleNext}
+          >
+            {currQues > questions.length - 2 ? "Submit" : "Next Question"}
+          </Button>
+        </div>
+      </div>}
     </div>
   );
 };
